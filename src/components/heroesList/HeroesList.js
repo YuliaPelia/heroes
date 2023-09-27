530b85dc63317ddc79bd033fa2224203d64f7811
Yulchikss <yuliapelia19@gmail.com>
2023-09-27 15:10:10 +0300

    create func for delete heroes

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,10 +1,15 @@
-import {useHttp} from '../../hooks/http.hook';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { heroesFetching, heroesFetched, heroesFetchingError } from '../../actions';
+import { useHttp } from "../../hooks/http.hook";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { connect } from "react-redux";
+import { deleteItem } from "../../actions";
+import {
+  heroesFetching,
+  heroesFetched,
+  heroesFetchingError,
+} from "../../actions";
 import HeroesListItem from "../heroesListItem/HeroesListItem";
-import Spinner from '../spinner/Spinner';
+import Spinner from "../spinner/Spinner";
 
 // Задача для этого компонента:
 // При клике на "крестик" идет удаление персонажа из общего состояния
@@ -12,41 +17,67 @@ import Spinner from '../spinner/Spinner';
 // Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
-    const {heroes, heroesLoadingStatus} = useSelector(state => state);
-    const dispatch = useDispatch();
-    const {request} = useHttp();
+  const { heroes, heroesLoadingStatus } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const { request } = useHttp();
 
-    useEffect(() => {
-        dispatch(heroesFetching());
-        request("http://localhost:3001/heroes")
-            .then(data => dispatch(heroesFetched(data)))
-            .catch(() => dispatch(heroesFetchingError()))
+  useEffect(() => {
+    dispatch(heroesFetching());
+    request("http://localhost:3001/heroes")
+      .then((data) => {
+        console.log(data);
+        // dispatch(deleteItem(data)); // my
+        dispatch(heroesFetched(data));
+      })
+      .catch(() => dispatch(heroesFetchingError()));
 
-        // eslint-disable-next-line
-    }, []);
+    // eslint-disable-next-line
+  }, []);
+  const deleteHero = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3001/heroes/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    if (heroesLoadingStatus === "loading") {
-        return <Spinner/>;
-    } else if (heroesLoadingStatus === "error") {
-        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+      if (response.ok) {
+        deleteItem(id);
+        console.log("cools");
+      } else {
+        console.error("Помилка видалення героя");
+      }
+    } catch (error) {
+      console.error("Помилка під час видалення героя", error);
+    }
+  };
+  if (heroesLoadingStatus === "loading") {
+    return <Spinner />;
+  } else if (heroesLoadingStatus === "error") {
+    return <h5 className="text-center mt-5">Ошибка загрузки</h5>;
+  }
+
+  const renderHeroesList = (arr) => {
+    if (arr.length === 0) {
+      return <h5 className="text-center mt-5">Героев пока нет</h5>;
     }
 
-    const renderHeroesList = (arr) => {
-        if (arr.length === 0) {
-            return <h5 className="text-center mt-5">Героев пока нет</h5>
-        }
+    return arr.map(({ id, ...props }) => {
+      return (
+        <HeroesListItem
+          key={id}
+          id={id}
+          {...props}
+          deleteItem={deleteItem}
+          deleteHero={deleteHero}
+        />
+      );
+    });
+  };
 
-        return arr.map(({id, ...props}) => {
-            return <HeroesListItem key={id} {...props}/>
-        })
-    }
+  const elements = renderHeroesList(heroes);
+  return <ul>{elements}</ul>;
+};
 
-    const elements = renderHeroesList(heroes);
-    return (
-        <ul>
-            {elements}
-        </ul>
-    )
-}
-
-export default HeroesList;
+export default connect(null, { deleteItem })(HeroesList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,29 +1,37 @@
 const initialState = {
-    heroes: [],
-    heroesLoadingStatus: 'idle',
-    filters: []
-}
+  heroes: [],
+  heroesLoadingStatus: "idle",
+  filters: [],
+};
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'HEROES_FETCHING':
-            return {
-                ...state,
-                heroesLoadingStatus: 'loading'
-            }
-        case 'HEROES_FETCHED':
-            return {
-                ...state,
-                heroes: action.payload,
-                heroesLoadingStatus: 'idle'
-            }
-        case 'HEROES_FETCHING_ERROR':
-            return {
-                ...state,
-                heroesLoadingStatus: 'error'
-            }
-        default: return state
-    }
-}
+  switch (action.type) {
+    case "HEROES_FETCHING":
+      return {
+        ...state,
+        heroesLoadingStatus: "loading",
+      };
+    case "HEROES_FETCHED":
+      return {
+        ...state,
+        heroes: action.payload,
+        heroesLoadingStatus: "idle",
+      };
+    case "HEROES_FETCHING_ERROR":
+      return {
+        ...state,
+        heroesLoadingStatus: "error",
+      };
+    case "DELETE_ITEM":
+      // console.log("Filtered heroes:", "done");
+
+      return {
+        ...state,
+        heroes: state.heroes.filter((hero) => hero.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
